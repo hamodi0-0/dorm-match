18131176d2b141bc416a0309c614b11c43537af1
hamodi0-0 <mh9988527@gmail.com>
2026-05-14 23:00:54 +0300

    Remove unnecassary loading files

--- app/dashboard/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageLoader } from "@/components/ui/page-loader";
-
-export default function DashboardLoading() {
-  return <PageLoader className="min-h-[50vh]" />;
-}
--- app/lister/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageLoader } from "@/components/ui/page-loader";
-
-export default function ListerLoading() {
-  return <PageLoader className="min-h-[50vh]" />;
-}
